259e2a0bc8d83973088cddb5949463a6a20ea355
heweixiao <840232857@qq.com>
2018-10-26 14:15:04 +0800

    server.js 修改

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const { exec } = require('child_process');
 const app = new express();
 app.use(express.static('./'))
 app.get('/', function(req, res) {
   res.render(__dirname + '/index.html');
 });
 app.get('/test',function(req, res) {
-  console.log(req);
+ exec('git pull ', (error, stdout, stderr) => {
+     if(error) {
+	console.log( 'pull error' + error);
+	return;
+	}
+	exec('pm2 restart 0',(err,stdout,stderr)=> {
+		if(err) {
+			console.log('restart err' + err);
+			return;
+		}
+		console.log('ok')
+	});	
+  }
+ );
+res.end('ok!')
 })
 app.listen(3000);
